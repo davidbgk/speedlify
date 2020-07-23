6f0810f624da0dfe4dd183ae2127f47ef88fcb8f
David Larlet <david@larlet.fr>
2020-07-23 14:37:55 -0400

    Update data management for custom sites

---
+++ _data/sites/perso.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: "Perso", // optional, falls back to object key
+	description: "Sites perso",
+	options: {
+		frequency: 60 * 24, // 24 hours
+		freshChrome: "site",
+	},
+	urls: [
+		"https://larlet.fr/david/",
+		"https://larlet.fr/david/2020/06/12/",
+		"https://larlet.com/",
+	]
+};
---
+++ _data/sites/pro.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: "Pro", // optional, falls back to object key
+	description: "Sites pro",
+	options: {
+		frequency: 60 * 24, // 24 hours
+		// if the sites don’t share assets on the same domain, we can reset
+		// chrome with each run instead of each site in every run (it’s faster)
+		freshChrome: "run"
+	},
+	urls: [
+		"http://scopyleft.fr/",
+		"https://mesconseilscovid.sante.gouv.fr/",
+		"http://demo.mesconseilscovid.fr/",
+	]
+};
