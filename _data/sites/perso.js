912fb50b58affa433fe9f68d1e326fdb717dc7e6
David Larlet <david@larlet.fr>
2020-07-24 08:18:53 -0400

    Add a link to custom cache page

--- _data/sites/perso.js
+++ _data/sites/perso.js
@@ -8,6 +8,7 @@ module.exports = {
 	urls: [
 		"https://larlet.fr/david/",
 		"https://larlet.fr/david/2020/06/12/",
+		"https://larlet.fr/david/cache/2020/a348ef62b74f25ff5e0679c67b3e6c3a/",
 		"https://larlet.com/",
 	]
 };
